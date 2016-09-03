cccdc4135cced372de9a46c796dea4e4ec84a84a
mzgajner <mato@zgajner.com>
2016-09-03 10:03:38 +0200

    Implement ballot filtering.

--- p/glasovanja/card/js/script.js
+++ p/glasovanja/card/js/script.js
@@ -1,3 +1,25 @@
-$('.tag-selector').select2({
-  dropdownParent: $('.card-glasovanja')
-})
+var voteElements = $('.votes li');
+
+$('.tag-selector')
+  .select2({
+    dropdownParent: $('.card-glasovanja')
+  })
+  .on('change', function(event) {
+    var index = 0,
+        activeTags = $(event.currentTarget).val(),
+        hideAll = false;
+
+    if (!activeTags) {
+      hideAll = true;
+    }
+    voteData.results.forEach(function(result) {
+      var dateRendered = false;
+      result.ballots.forEach(function(ballot) {
+        var shouldBeVisible = hideAll ? false : activeTags.indexOf(ballot.tags[0]) > -1
+        voteElements.eq(index).css({display : shouldBeVisible ? 'flex' : 'none' })
+                              .children('.date').css({visibility: shouldBeVisible && !dateRendered ? 'visible' : 'hidden'});
+        dateRendered = dateRendered || shouldBeVisible;
+        index++;
+      })
+    })
+  })
