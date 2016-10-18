e4c77140a654cb3dffe88c3e2750e8fce4fc973c
Filip Muki Dobranic <filipdobranic@gmail.com>
2016-10-18 18:56:15 +0200

    raba-skozi-cas

--- s/raba-skozi-cas/card/js/script.js
+++ s/raba-skozi-cas/card/js/script.js
@@ -1,214 +1,377 @@
-var mydata;
-var data = [];
+/* ========================================================================
+ * Bootstrap: tab.js v3.3.6
+ * http://getbootstrap.com/javascript/#tabs
+ * ========================================================================
+ * Copyright 2011-2016 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * ======================================================================== */
 
-// $.getJSON('https://isci.parlameter.si/q/zdravstvo', function(r) {
-    // console.log(r);
-    mydata = raba_data;
-    raw_data = raba_data['facet_counts']['facet_ranges']['datetime_dt'];
 
-    // console.log(raw_data);
++function ($) {
+  'use strict';
 
-    var dates = [];
-    var occurences = [];
+  // TAB CLASS DEFINITION
+  // ====================
 
-    var date_formatter = d3.time.format('%Y-%m-%dT%H:%M:%SZ');
+  var Tab = function (element) {
+    // jscs:disable requireDollarBeforejQueryAssignment
+    this.element = $(element)
+    // jscs:enable requireDollarBeforejQueryAssignment
+  }
 
-    // var data = []
+  Tab.VERSION = '3.3.6'
 
-    var ticks = []
+  Tab.TRANSITION_DURATION = 150
 
-    for (i = 0; i < raw_data['counts'].length; i++) {
-        if (i % 2 === 0 && i > raw_data['counts'].length - 25) {
-            dates.push(date_formatter.parse(raw_data['counts'][i]));
-            occurences.push(raw_data['counts'][i + 1]);
-            data.push({
-                'date': raw_data['counts'][i],
-                'occurences': raw_data['counts'][i + 1]
-            });
-        }
+  Tab.prototype.show = function () {
+    var $this    = this.element
+    var $ul      = $this.closest('ul:not(.dropdown-menu)')
+    var selector = $this.data('target')
+
+    if (!selector) {
+      selector = $this.attr('href')
+      selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
     }
 
-    data.sort(function(x, y) {
-        return date_formatter.parse(x.date) - date_formatter.parse(y.date);
-    });
+    if ($this.parent('li').hasClass('active')) return
 
-    // console.log(data);
+    var $previous = $ul.find('.active:last a')
+    var hideEvent = $.Event('hide.bs.tab', {
+      relatedTarget: $this[0]
+    })
+    var showEvent = $.Event('show.bs.tab', {
+      relatedTarget: $previous[0]
+    })
 
-    // for (piece in data) {
-    //     if (piece % 2 === 0) {
-    //         ticks.push(date_formatter.parse(data[piece].date));
-    //     }
-    // }
+    $previous.trigger(hideEvent)
+    $this.trigger(showEvent)
 
-    var margin = {
-        top: 30,
-        right: 30,
-        bottom: 30,
-        left: 30
-    };
-    var width = 960 - margin.left - margin.right;
-    var height = 400 - margin.top - margin.bottom;
+    if (showEvent.isDefaultPrevented() || hideEvent.isDefaultPrevented()) return
 
-    var parseDate = d3.time.format("%Y-%m-%dT%H:%M:%SZ").parse;
+    var $target = $(selector)
 
-    var SI = d3.locale({
-        "decimal": ",",
-        "thousands": " ",
-        "grouping": [3],
-        "currency": ["EUR", ""],
-        "dateTime": "%d. %m. %Y %H:%M",
-        "date": "%d. %m. %Y",
-        "time": "%H:%M:%S",
-        "periods": ["AM", "PM"],
-        "days": ["nedelja", "ponedeljek", "torek", "sreda", "četrtek", "petek", "sobota"],
-        "shortDays": ["ned", "pon", "tor", "sre", "čet", "pet", "sob"],
-        "months": ["januar", "februar", "marec", "april", "maj", "junij", "julij", "avgust", "september", "oktober", "november", "december"],
-        "shortMonths": ["jan", "feb", "mar", "apr", "maj", "jun", "jul", "avg", "sep", "okt", "nov", "dec"]
-    });
+    this.activate($this.closest('li'), $ul)
+    this.activate($target, $target.parent(), function () {
+      $previous.trigger({
+        type: 'hidden.bs.tab',
+        relatedTarget: $this[0]
+      })
+      $this.trigger({
+        type: 'shown.bs.tab',
+        relatedTarget: $previous[0]
+      })
+    })
+  }
 
-    console.log(width, data.length);
+  Tab.prototype.activate = function (element, container, callback) {
+    var $active    = container.find('> .active')
+    var transition = callback
+      && $.support.transition
+      && ($active.length && $active.hasClass('fade') || !!container.find('> .fade').length)
 
-    var x = d3.time.scale()
-        .range([0, width]);
-    // var x = d3.time.scale().nice(12).range([0, width]);
-    // var x = d3.scale.ordinal().domain(dates).range([0, width]);
+    function next() {
+      $active
+        .removeClass('active')
+        .find('> .dropdown-menu > .active')
+          .removeClass('active')
+        .end()
+        .find('[data-toggle="tab"]')
+          .attr('aria-expanded', false)
+
+      element
+        .addClass('active')
+        .find('[data-toggle="tab"]')
+          .attr('aria-expanded', true)
+
+      if (transition) {
+        element[0].offsetWidth // reflow for transition
+        element.addClass('in')
+      } else {
+        element.removeClass('fade')
+      }
+
+      if (element.parent('.dropdown-menu').length) {
+        element
+          .closest('li.dropdown')
+            .addClass('active')
+          .end()
+          .find('[data-toggle="tab"]')
+            .attr('aria-expanded', true)
+      }
+
+      callback && callback()
+    }
+
+    $active.length && transition ?
+      $active
+        .one('bsTransitionEnd', next)
+        .emulateTransitionEnd(Tab.TRANSITION_DURATION) :
+      next()
+
+    $active.removeClass('in')
+  }
+
+
+  // TAB PLUGIN DEFINITION
+  // =====================
+
+  function Plugin(option) {
+    return this.each(function () {
+      var $this = $(this)
+      var data  = $this.data('bs.tab')
+
+      if (!data) $this.data('bs.tab', (data = new Tab(this)))
+      if (typeof option == 'string') data[option]()
+    })
+  }
+
+  var old = $.fn.tab
+
+  $.fn.tab             = Plugin
+  $.fn.tab.Constructor = Tab
+
+
+  // TAB NO CONFLICT
+  // ===============
+
+  $.fn.tab.noConflict = function () {
+    $.fn.tab = old
+    return this
+  }
+
+
+  // TAB DATA-API
+  // ============
+
+  var clickHandler = function (e) {
+    e.preventDefault()
+    Plugin.call($(this), 'show')
+  }
+
+  $(document)
+    .on('click.bs.tab.data-api', '[data-toggle="tab"]', clickHandler)
+    .on('click.bs.tab.data-api', '[data-toggle="pill"]', clickHandler)
+
+}(jQuery);
+
+/* end of bootstrap tabs */
+
+var mydata;
+var data = [];
+var smalldata = [];
+var bigdata = [];
+
+mydata = raba_data;
+raw_data = raba_data['facet_counts']['facet_ranges']['datetime_dt'];
+
+var date_formatter = d3.time.format('%Y-%m-%dT%H:%M:%SZ');
+
+var ticks = []
+
+// create small data
+var dates = [];
+var occurences = [];
+for (i = 0; i < raw_data['counts'].length; i++) {
+    if (i % 2 === 0 && i > raw_data['counts'].length - 25) {
+        dates.push(date_formatter.parse(raw_data['counts'][i]));
+        occurences.push(raw_data['counts'][i + 1]);
+        data.push({
+            'date': raw_data['counts'][i],
+            'occurences': raw_data['counts'][i + 1]
+        });
+        smalldata.push({
+            'date': raw_data['counts'][i],
+            'occurences': raw_data['counts'][i + 1]
+        });
+    }
+}
+
+// create big data
+var dates = [];
+var occurences = [];
+for (i = 0; i < raw_data['counts'].length; i++) {
+    if (i % 2 === 0) {
+        dates.push(date_formatter.parse(raw_data['counts'][i]));
+        occurences.push(raw_data['counts'][i + 1]);
+        bigdata.push({
+            'date': raw_data['counts'][i],
+            'occurences': raw_data['counts'][i + 1]
+        });
+    }
+}
+
+data.sort(function(x, y) {
+    return date_formatter.parse(x.date) - date_formatter.parse(y.date);
+});
+smalldata.sort(function(x, y) {
+    return date_formatter.parse(x.date) - date_formatter.parse(y.date);
+});
+bigdata.sort(function(x, y) {
+    return date_formatter.parse(x.date) - date_formatter.parse(y.date);
+});
+
+// global stuff for the chart
+var margin = {
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 30
+};
+var width = 960 - margin.left - margin.right;
+var height = 400 - margin.top - margin.bottom;
+
+var SI = d3.locale({
+    "decimal": ",",
+    "thousands": " ",
+    "grouping": [3],
+    "currency": ["EUR", ""],
+    "dateTime": "%d. %m. %Y %H:%M",
+    "date": "%d. %m. %Y",
+    "time": "%H:%M:%S",
+    "periods": ["AM", "PM"],
+    "days": ["nedelja", "ponedeljek", "torek", "sreda", "četrtek", "petek", "sobota"],
+    "shortDays": ["ned", "pon", "tor", "sre", "čet", "pet", "sob"],
+    "months": ["januar", "februar", "marec", "april", "maj", "junij", "julij", "avgust", "september", "oktober", "november", "december"],
+    "shortMonths": ["jan", "feb", "mar", "apr", "maj", "jun", "jul", "avg", "sep", "okt", "nov", "dec"]
+});
+
+var parseDate = d3.time.format("%Y-%m-%dT%H:%M:%SZ").parse;
+data.forEach(function(d) {
+    d.date = parseDate(d.date);
+    d.occurences = +d.occurences;
+});
+smalldata.forEach(function(d) {
+    d.date = parseDate(d.date);
+    d.occurences = +d.occurences;
+});
+bigdata.forEach(function(d) {
+    d.date = parseDate(d.date);
+    d.occurences = +d.occurences;
+});
+
+var svg = d3.select(".timechart").append("svg")
+    .attr('class', 'smalldata')
+    .attr('viewBox', '0 0 960 400')
+    .attr('preserveAspectRatio', 'xMidYMid meet')
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+function createSmallChart(data) {
+
+    svg.selectAll(".bigbarcontainer").remove();
+    svg.selectAll(".axis").remove();
+
+    var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
 
     var y = d3.scale.linear()
         .range([height, 0]);
 
+    x.domain(data.map(function(d) { return d.date; }));
+    y.domain([0, d3.max(data, function(d) { return d.occurences; })]);
+
     var xAxis = d3.svg.axis()
         .scale(x)
         .orient("bottom")
-        .ticks(data.length / 2)
         .tickFormat(SI.timeFormat('%b %y'));
-    // .tickValues(ticks);
 
     var yAxis = d3.svg.axis()
         .scale(y)
         .orient("left");
-
-    // var line = d3.svg.line()
-    //     .x(function(d) {
-    //         // console.log('date' + x(d.date));
-    //         return x(d.date);
-    //     })
-    //     .y(function(d) {
-    //         // console.log(x(d.occurences));
-    //         return y(d.occurences);
-    //     });
-
-    var svg = d3.select(".timechart").append("svg")
-        // .attr("width", width + margin.left + margin.right)
-        // .attr("height", height + margin.top + margin.bottom)
-        .attr('viewBox', '0 0 960 400')
-        .attr('preserveAspectRatio', 'xMidYMid meet')
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    data.forEach(function(d) {
-        // console.log(d);
-        // console.log(d.date);
-        // console.log(parseDate(d.date));
-        d.date = parseDate(d.date);
-        // console.log(d.date);
-        d.occurences = +d.occurences;
-    });
-
-    x.domain(d3.extent(data, function(d) {
-        return d.date;
-    }));
-    y.domain(d3.extent(data, function(d) {
-        return d.occurences;
-    }));
 
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
 
-    // console.log('ping');
-
-    svg.selectAll(".bar")
+    var barcontainers = svg.selectAll(".smallbarcontainer")
         .data(data)
-        .enter().append("rect")
+        .enter()
+        .append('g')
+        .attr('class', 'smallbarcontainer');
+
+    barcontainers.append('text')
+        .text(function(d) {
+            return d.occurences;
+        })
+        .attr("x", function(d) {
+            return x(d.date);
+        })
+        .attr("y", function(d) {
+            return y(d.occurences);
+        })
+        .attr('width', x.rangeBand)
+        .attr('text-anchor', 'middle')
+        .attr('transform', 'translate(' + x.rangeBand()/2 + ', -4)');
+
+    barcontainers.append("rect")
         .attr("class", "bar")
         .attr("x", function(d) {
             return x(d.date);
         })
-        .attr("width", width / data.length)
+        .attr("width", x.rangeBand())
         .attr("y", function(d) {
             return y(d.occurences);
         })
         .attr("height", function(d) {
             return height - y(d.occurences);
         });
+}
 
-    // svg.append("g")
-    //     .attr("class", "y axis")
-    //     .call(yAxis)
-    //     .append("text")
-    //     .attr("transform", "rotate(-90)")
-    //     .attr("y", 6)
-    //     .attr("dy", ".71em")
-    //     .style("text-anchor", "end")
-    //     .text("Število pojavitev");
+function createBigChart(data) {
 
-    // console.log('ping2');
+    svg.selectAll(".smallbarcontainer").remove();
+    svg.selectAll(".axis").remove();
 
-    // svg.append("path")
-    //     .datum(data)
-    //     .attr("class", "line")
-    //     .attr("d", line);
+    var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
 
-    // console.log('ping3');
+    var y = d3.scale.linear()
+        .range([height, 0]);
 
-    // svg.selectAll("g.dot")
-    //     .data(data)
-    //     .enter()
-    //     .append("g")
-    //     .attr("class", "dot")
-    //     .append("circle")
-    //     .attr("r", 11)
-    //     .attr("cx", function(d, i) {
-    //         // console.log(d.date);
-    //         return x(d.date);
-    //     })
-    //     .attr("cy", function(d, i) {
-    //         return y(d.occurences);
-    //     });
-    //
-    // svg.selectAll('g.dot')
-    //     .data(data)
-    //     .append('text')
-    //     // .attr('dx', function(d) {
-    //     //     return -20
-    //     // })
-    //     // .attr("cx", function(d, i) {
-    //     //     // console.log(d.date);
-    //     //     return x(d.date);
-    //     // })
-    //     // .attr("cy", function(d, i) {
-    //     //     return y(d.occurences);
-    //     // })
-    //     .attr('transform', function(d) {
-    //         return 'translate(' + (x(d.date) - 8) + ',' + (y(d.occurences) + 5) + ')'
-    //     })
-    //     .text(function(d) {
-    //         return d.occurences
-    //     });
+    x.domain(data.map(function(d) { return d.date; }));
+    y.domain([0, d3.max(data, function(d) { return d.occurences; })]);
 
-    // var aspect = width / height,
-    //     chart = d3.select('#chart');
-    // d3.select(window)
-    //     .on("resize", function() {
-    //         var targetWidth = chart.node().getBoundingClientRect().width;
-    //         chart.attr("width", targetWidth);
-    //         chart.attr("height", targetWidth / aspect);
-    //     });
-// });
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .tickValues(x.domain().filter(function(d, i) { return !(i % 3); }))
+        .tickFormat(SI.timeFormat('%b %y'));
 
-// function type(d) {
-//     d.date = parseDate.parse(d.date);
-//     d.occurences = +d.occurences;
-//     return d;
-// }
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    var barcontainers = svg.selectAll(".bigbarcontainer")
+        .remove()
+        .data(data)
+        .enter()
+        .append('g')
+        .attr('class', 'bigbarcontainer');
+
+    barcontainers.append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) {
+            return x(d.date);
+        })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) {
+            return y(d.occurences);
+        })
+        .attr("height", function(d) {
+            return height - y(d.occurences);
+        });
+}
+
+createSmallChart(smalldata);
+
+$('#smalldata' + random_id).on('click', function() {
+    createSmallChart(smalldata);
+});
+$('#bigdata' + random_id).on('click', function() {
+    createBigChart(bigdata);
+});
