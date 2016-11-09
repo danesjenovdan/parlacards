e8498f0c1e8bd7c580e73e62ea05e9e14757363c
Filip Muki Dobranic <filipdobranic@gmail.com>
2016-11-09 12:07:25 +0100

    zadnja seja fixes

--- c/zadnja-seja/card/js/script.js
+++ c/zadnja-seja/card/js/script.js
@@ -1,13 +1,13 @@
 function equalizeWidth() {
     var max = 0;
-    var iterations = +$('.term').length;
+    var iterations = +$('.' + className + ' .term').length;
     $.each($('.' + className + ' .term'), function(i, e) {
         if ($(e).width() > max) {
             max = $(e).width();
         }
         if (i === iterations - 1) {
             $('.' + className + ' .term').css('width', max);
-            $('.frequency').css('width', $('.' + className + ' .term').parent().width() - max - 10);
+            $('.' + className + ' .frequency').css('width', $('.' + className + ' .term').parent().width() - max - 10);
             console.log('ping');
         }
     });
