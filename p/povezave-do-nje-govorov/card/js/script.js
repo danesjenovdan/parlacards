9af250f329ef4236e555190b14c2cf7dba59096d
Filip Muki Dobranic <filipdobranic@gmail.com>
2016-11-20 19:38:56 +0100

    povezave do govorov poslanca date fix

--- p/povezave-do-nje-govorov/card/js/script.js
+++ p/povezave-do-nje-govorov/card/js/script.js
@@ -2,3 +2,24 @@ addCardFlip();
 makeEmbedSwitch();
 activateCopyButton();
 addCardRippling();
+
+// $.each($('.date'), function(i ,e) {
+//     $(e).data('offset', $(e).position().top);
+// });
+// $('.stickinme').on('scroll', function(i, e) {
+//     $.each($('.date'), function(i, e) {
+//         if ($(e).position().top <= 0) {
+//             $(e).css({
+//                 'top': - $(e).parent().position().top - $(e).data('offset')
+//             });
+//         } else if ($(e).position().top <= - $(e).parent().position().top - $(e).data('offset')) {
+//             $(e).css({
+//                 'top': - $(e).parent().position().top - $(e).data('offset')
+//             });
+//         } else {
+//             $(e).css({
+//                 'top': 0
+//             });
+//         }
+//     });
+// });
