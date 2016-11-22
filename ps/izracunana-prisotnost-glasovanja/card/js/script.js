061eebefb49a1c01282fc57bfa5b159ca46e14c3
Filip Muki Dobranic <filipdobranic@gmail.com>
2016-11-22 12:08:34 +0100

    stuff

---
+++ ps/izracunana-prisotnost-glasovanja/card/js/script.js
@@ -0,0 +1,16 @@
+function fixImages() {
+    $.each($('.smallbar .funblue'), function(i, e) {
+        if ($(e).width() <= 30) {
+            $(e).children().children('.avgminimg').css({
+                'margin-left': '0'
+            });
+            console.log($(e).children().children('.avgminimg'));
+        }
+    });
+}
+
+progressbarTooltip.init(className);
+
+makeEmbedSwitch();
+activateCopyButton();
+addCardRippling();
---
+++ ps/osnovne-informacije-poslanska-skupina/card/js/script.js
@@ -0,0 +1,4 @@
+makeEmbedSwitch();
+activateCopyButton();
+
+addCardRippling();
