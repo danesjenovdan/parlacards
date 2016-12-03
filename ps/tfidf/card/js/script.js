d159228b288a5eedd05bd0177d28c5d564f201c6
Filip Muki Dobranic <filipdobranic@gmail.com>
2016-12-03 16:10:19 +0100

    so many fixes

--- ps/tfidf/card/js/script.js
+++ ps/tfidf/card/js/script.js
@@ -1,25 +1,25 @@
-// $(document).ready(function() {
-    function equalizeWidth() {
-        var max = 0;
-        var iterations = +$('.term').length;
-        $.each($('.term'), function(i, e) {
-            if ($(e).width() > max) {
-                max = $(e).width();
-            }
-            if (i === iterations - 1) {
-                $('.term').css('width', max);
-                $('.frequency').css('width', $('.term').parent().width() - max - 10);
-                console.log('ping');
-            }
-        });
-    }
+// // $(document).ready(function() {
+//     function equalizeWidth() {
+//         var max = 0;
+//         var iterations = +$('.term').length;
+//         $.each($('.term'), function(i, e) {
+//             if ($(e).width() > max) {
+//                 max = $(e).width();
+//             }
+//             if (i === iterations - 1) {
+//                 $('.term').css('width', max);
+//                 $('.frequency').css('width', $('.term').parent().width() - max - 10);
+//                 console.log('ping');
+//             }
+//         });
+//     }
 
+// // });
+
+// equalizeWidth();
+// $(window).on('resize', function() {
+//     equalizeWidth();
 // });
-
-equalizeWidth();
-$(window).on('resize', function() {
-    equalizeWidth();
-});
 
 makeEmbedSwitch();
 activateCopyButton();
