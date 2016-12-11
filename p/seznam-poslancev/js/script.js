45f5efddcdae39624f6a4b96da9767448327c5b9
mzgajner <mato@zgajner.com>
2016-12-11 02:28:13 +0100

    Lint p/seznam-poslancev to test added functionality.

--- p/seznam-poslancev/js/script.js
+++ p/seznam-poslancev/js/script.js
@@ -1,49 +1,43 @@
-(function() {
-  var personListElement = $('.card-seznam-poslancev .person-list'),
-      headersElement = personListElement.find('.headers')
-      personElements = personListElement.children('.person'),
-      infoTextElement = $('.card-seznam-poslancev .info-text'),
-      currentSortProperty = 'name',
-      currentReverseOrder = false,
-      sortPropGetters = {
-        name: function(item) { return item.person.name },
-        party: function(item) { return item.person.party.acronym },
-        district: function(item) { return item.person.formattedDistrict },
-        analysis: function(item) { return item.results[seznamPoslancevState.analysis] },
-      };
+/* global $ seznamPoslancevState seznamPoslancevData seznamPoslancevColumns makeEmbedSwitch
+activateCopyButton addCardRippling */
 
-  headersElement.on('click', '.column', function(event) {
-    var headerElement = $(event.currentTarget),
-        sortProperty = headerElement.data('sort'),
-        reverseOrder = currentSortProperty === sortProperty && !currentReverseOrder;
-
-    updateInfoText(sortProperty);
-    sortBy(sortPropGetters[sortProperty], reverseOrder);
-    toggleClasses(headerElement, sortProperty, currentSortProperty);
-
-    currentSortProperty = sortProperty;
-    currentReverseOrder = reverseOrder;
-  });
+(() => {
+  const personListElement = $('.card-seznam-poslancev .person-list');
+  const headersElement = personListElement.find('.headers');
+  let personElements = personListElement.children('.person');
+  const infoTextElement = $('.card-seznam-poslancev .info-text');
+  let currentSortProperty = 'name';
+  let currentReverseOrder = false;
+  const sortPropGetters = {
+    name: item => item.person.name,
+    party: item => item.person.party.acronym,
+    district: item => item.person.formattedDistrict,
+    analysis: item => item.results[seznamPoslancevState.analysis],
+  };
 
   function toggleClasses(element, newSortProperty, oldSortProperty) {
     if (newSortProperty === oldSortProperty) {
       element.toggleClass('reverse');
-    }
-    else {
+    } else {
       headersElement.find('.sort, .reverse').removeClass('sort reverse');
-      element.addClass('sort')
+      element.addClass('sort');
     }
   }
 
   function sortBy(propGetter, reverseOrder) {
-    personElements.sort(function(a,b) {
-      var an = propGetter(seznamPoslancevData.data[a.getAttribute('data-index')]),
-          bn = propGetter(seznamPoslancevData.data[b.getAttribute('data-index')]);
+    personElements.sort((a, b) => {
+      const an = propGetter(seznamPoslancevData.data[a.getAttribute('data-index')]);
+      const bn = propGetter(seznamPoslancevData.data[b.getAttribute('data-index')]);
 
-      return an > bn ? 1 : (an < bn ? -1 : 0)
+      if (an > bn) {
+        return 1;
+      } else if (an < bn) {
+        return -1;
+      }
+      return 0;
     });
 
-    personElements.detach()
+    personElements.detach();
 
     if (reverseOrder) {
       personElements = $(personElements.get().reverse());
@@ -52,39 +46,50 @@
     personElements.appendTo(personListElement);
   }
 
+  function getInfoText(sortProperty) {
+    function getDistrictNames(ids) {
+      return seznamPoslancevData.districts
+        .filter(district => ids.indexOf(String(Object.keys(district)[0])) > -1)
+        .map(district => district[String(Object.keys(district)[0])]);
+    }
+
+    const parties = seznamPoslancevState.parties ? `poslanska skupina: ${seznamPoslancevState.parties.join(', ')}` : 'vse poslanske skupine';
+    const districts = seznamPoslancevState.districts ? `volilni okraj: ${getDistrictNames(seznamPoslancevState.districts).join(', ')}` : 'vsi volilni okraji';
+    const presiding = seznamPoslancevState.excludePresiding ? '; brez predsedujočih poslancev' : '';
+    const firstLine = `Množica vseh trenutno aktivnih poslancev, ki ustrezajo uporabniškemu vnosu (${parties}; ${districts + presiding}).`;
+
+    const sortMap = {
+      name: 'abecedi',
+      district: 'okrajih',
+      party: 'poslanskih skupinah',
+      analysis: `rezultatu analize${seznamPoslancevState && seznamPoslancevState.analysis ? seznamPoslancevColumns[seznamPoslancevState.analysis].label : ''}`,
+    };
+
+    const secondLine = `Seznam je sortiran po ${sortMap[sortProperty]}.`;
+    let thirdLine;
+    if (seznamPoslancevState && seznamPoslancevState.analysis) {
+      thirdLine = seznamPoslancevColumns[seznamPoslancevState.analysis].explanation;
+    }
+
+    return `${firstLine}<br><br>${secondLine}${thirdLine ? `<br><br>${thirdLine}` : ''}`;
+  }
+
   function updateInfoText(sortProperty) {
     infoTextElement.html(getInfoText(sortProperty));
   }
 
-  function getInfoText(sortProperty) {
-    function getDistrictNames(ids) {
-      return seznamPoslancevData.districts
-        .filter(function(district) {
-          return ids.indexOf(String(Object.keys(district)[0])) > -1;
-        })
-        .map(function(district) {
-          return district[String(Object.keys(district)[0])];
-        });
-    }
+  headersElement.on('click', '.column', (event) => {
+    const headerElement = $(event.currentTarget);
+    const sortProperty = headerElement.data('sort');
+    const reverseOrder = currentSortProperty === sortProperty && !currentReverseOrder;
 
-    var parties = seznamPoslancevState.parties ? 'poslanska skupina: ' + seznamPoslancevState.parties.join(', ') : 'vse poslanske skupine';
-    var districts = seznamPoslancevState.districts ? 'volilni okraj: ' + getDistrictNames(seznamPoslancevState.districts).join(', ') : 'vsi volilni okraji';
-    var presiding = seznamPoslancevState.excludePresiding ? '; brez predsedujočih poslancev' : '';
-    var firstLine = 'Množica vseh trenutno aktivnih poslancev, ki ustrezajo uporabniškemu vnosu (' + parties + '; ' + districts + presiding + ').';
+    updateInfoText(sortProperty);
+    sortBy(sortPropGetters[sortProperty], reverseOrder);
+    toggleClasses(headerElement, sortProperty, currentSortProperty);
 
-    var sortMap = {
-      name: 'abecedi',
-      district: 'okrajih',
-      party: 'poslanskih skupinah',
-      analysis: 'rezultatu analize ' + (seznamPoslancevState && seznamPoslancevState.analysis ? seznamPoslancevColumns[seznamPoslancevState.analysis].label : '')
-    };
-
-    var secondLine = 'Seznam je sortiran po ' + sortMap[sortProperty] + '.';
-
-    var thirdLine = seznamPoslancevState && seznamPoslancevState.analysis ? seznamPoslancevColumns[seznamPoslancevState.analysis].explanation : null;
-
-    return firstLine + '<br><br>' + secondLine + (thirdLine ? '<br><br>' + thirdLine : '');
-  };
+    currentSortProperty = sortProperty;
+    currentReverseOrder = reverseOrder;
+  });
 
   updateInfoText('name');
   makeEmbedSwitch();
