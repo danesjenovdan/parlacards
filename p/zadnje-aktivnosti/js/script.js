2d5542af151ff41cfd6549502faecd48a6c49fd9
Filip Muki Dobranic <filipdobranic@gmail.com>
2017-01-16 17:39:15 +0100

    killed sticky header on last activity

--- p/zadnje-aktivnosti/js/script.js
+++ p/zadnje-aktivnosti/js/script.js
@@ -3,23 +3,23 @@ activateCopyButton();
 addCardRippling();
 
 
-$.each($('.date'), function(i ,e) {
-    $(e).data('offset', $(e).position().top);
-});
-$('.stickinme').on('scroll', function(i, e) {
-    $.each($('.date'), function(i, e) {
-        if ($(e).position().top <= 0) {
-            $(e).css({
-                'top': - $(e).parent().position().top - $(e).data('offset')
-            });
-        } else if ($(e).position().top <= - $(e).parent().position().top - $(e).data('offset')) {
-            $(e).css({
-                'top': - $(e).parent().position().top - $(e).data('offset')
-            });
-        } else {
-            $(e).css({
-                'top': 0
-            });
-        }
-    });
-});
+// $.each($('.date'), function(i ,e) {
+//     $(e).data('offset', $(e).position().top);
+// });
+// $('.stickinme').on('scroll', function(i, e) {
+//     $.each($('.date'), function(i, e) {
+//         if ($(e).position().top <= 0) {
+//             $(e).css({
+//                 'top': - $(e).parent().position().top - $(e).data('offset')
+//             });
+//         } else if ($(e).position().top <= - $(e).parent().position().top - $(e).data('offset')) {
+//             $(e).css({
+//                 'top': - $(e).parent().position().top - $(e).data('offset')
+//             });
+//         } else {
+//             $(e).css({
+//                 'top': 0
+//             });
+//         }
+//     });
+// });
