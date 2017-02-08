710b58d1303bcbfb8564d0e89d0baa7e77e99e9a
mzgajner <mato@zgajner.com>
2017-02-08 18:16:40 +0100

    Filter dropdowns according to text filter.

--- p/glasovanja/js/script.js
+++ p/glasovanja/js/script.js
@@ -14,6 +14,28 @@
       monthPlaceholder() {
         return this.selectedMonths.length > 0 ? `Izbranih: ${this.selectedMonths.length}` : 'Izberi';
       },
+      dropdownItems() {
+        const validTags = [];
+        const validMonths = [];
+
+        this.getFilteredVotingDays(true).forEach((votingDay) => {
+          const [, month, year] = votingDay.date.split(' ').map(string => parseInt(string, 10));
+          const monthId = `${year}-${month}`;
+          if (validMonths.indexOf(monthId) === -1) validMonths.push(monthId);
+
+          votingDay.ballots
+            .forEach((ballot) => {
+              ballot.tags.forEach((tag) => {
+                if (validTags.indexOf(tag) === -1) validTags.push(tag);
+              });
+            });
+        });
+
+        return {
+          tags: this.allTags.filter(tag => validTags.indexOf(tag.id) > -1),
+          months: this.allMonths.filter(month => validMonths.indexOf(month.id) > -1),
+        };
+      },
       selectedTags() {
         return this.allTags
           .filter(tag => tag.selected)
@@ -27,12 +49,52 @@
                               .map(option => option.id);
       },
       filteredVotingDays() {
+        return this.getFilteredVotingDays();
+      },
+    },
+    data() {
+      return {
+        allTags: voteData.all_tags.map(tag => ({ id: tag, label: tag, selected: false })),
+        allOptions: [
+          { id: 'za', class: 'for', label: 'ZA', selected: false },
+          { id: 'proti', class: 'against', label: 'PROTI', selected: false },
+          { id: 'kvorum', class: 'kvorum', label: 'VZDRŽAN', selected: false },
+          { id: 'ni', class: 'ni', label: 'NI', selected: false },
+        ],
+        allMonths: [
+          { id: '2017-2', label: 'Februar 2017', month: 2, year: 2017, selected: false },
+          { id: '2017-1', label: 'Januar 2017', month: 1, year: 2017, selected: false },
+          { id: '2016-12', label: 'December 2016', month: 12, year: 2016, selected: false },
+          { id: '2016-11', label: 'November 2016', month: 11, year: 2016, selected: false },
+          { id: '2016-10', label: 'Oktober 2016', month: 10, year: 2016, selected: false },
+          { id: '2016-9', label: 'September 2016', month: 9, year: 2016, selected: false },
+          { id: '2016-8', label: 'Avgust 2016', month: 8, year: 2016, selected: false },
+          { id: '2016-7', label: 'Julij 2016', month: 7, year: 2016, selected: false },
+          { id: '2016-6', label: 'Junij 2016', month: 6, year: 2016, selected: false },
+          { id: '2016-5', label: 'Maj 2016', month: 5, year: 2016, selected: false },
+          { id: '2016-4', label: 'April 2016', month: 4, year: 2016, selected: false },
+          { id: '2016-3', label: 'Marec 2016', month: 3, year: 2016, selected: false },
+          { id: '2016-2', label: 'Februar 2016', month: 2, year: 2016, selected: false },
+          { id: '2016-1', label: 'Januar 2016', month: 1, year: 2016, selected: false },
+        ],
+        votingDays: voteData.results,
+        person: voteData.person,
+        textFilter: '',
+        selectedOptions: [],
+      };
+    },
+    methods: {
+      toggleOption(optionId) {
+        const clickedOption = this.allOptions.filter(option => option.id === optionId)[0];
+        clickedOption.selected = !clickedOption.selected;
+      },
+      getFilteredVotingDays(onlyFilterByText = false) {
         const filterBallots = (ballot) => {
-          const tagMatch = this.selectedTags.length === 0 ||
+          const tagMatch = onlyFilterByText || this.selectedTags.length === 0 ||
             ballot.tags.filter(tag => this.selectedTags.indexOf(tag) > -1).length > 0;
           const textMatch = this.textFilter === '' ||
             ballot.motion.toLowerCase().indexOf(this.textFilter.toLowerCase()) > -1;
-          const optionMatch = this.selectedOptions.length === 0 ||
+          const optionMatch = onlyFilterByText || this.selectedOptions.length === 0 ||
             this.selectedOptions.indexOf(ballot.option) > -1;
 
           return tagMatch && textMatch && optionMatch;
@@ -60,7 +122,7 @@
                 }
 
                 if (ballot.result !== 'none') {
-                  ballotClone.outcome = ballot.result === true ? 'Predlog sprejet' : 'Predlog zavrnjen'
+                  ballotClone.outcome = ballot.result === true ? 'Predlog sprejet' : 'Predlog zavrnjen';
                 }
 
                 return ballotClone;
@@ -68,42 +130,6 @@
           }))
           .filter(votingDay => votingDay.ballots.length > 0)
           .filter(filterDates);
-      },
-    },
-    data() {
-      return {
-        allTags: voteData.all_tags.map(tag => ({ id: tag, label: tag, selected: false })),
-        allOptions: [
-          { id: 'za', class: 'for', label: 'ZA', selected: false },
-          { id: 'proti', class: 'against', label: 'PROTI', selected: false },
-          { id: 'kvorum', class: 'kvorum', label: 'VZDRŽAN', selected: false },
-          { id: 'ni', class: 'ni', label: 'NI', selected: false },
-        ],
-        allMonths: [
-          { id: 'jan17', label: 'Januar 2017', month: 1, year: 2017, selected: false },
-          { id: 'dec16', label: 'December 2016', month: 12, year: 2016, selected: false },
-          { id: 'nov16', label: 'November 2016', month: 11, year: 2016, selected: false },
-          { id: 'okt16', label: 'Oktober 2016', month: 10, year: 2016, selected: false },
-          { id: 'sep16', label: 'September 2016', month: 9, year: 2016, selected: false },
-          { id: 'avg16', label: 'Avgust 2016', month: 8, year: 2016, selected: false },
-          { id: 'jul16', label: 'Julij 2016', month: 7, year: 2016, selected: false },
-          { id: 'jun16', label: 'Junij 2016', month: 6, year: 2016, selected: false },
-          { id: 'maj16', label: 'Maj 2016', month: 5, year: 2016, selected: false },
-          { id: 'apr16', label: 'April 2016', month: 4, year: 2016, selected: false },
-          { id: 'mar16', label: 'Marec 2016', month: 3, year: 2016, selected: false },
-          { id: 'feb16', label: 'Februar 2016', month: 2, year: 2016, selected: false },
-          { id: 'jan16', label: 'Januar 2016', month: 1, year: 2016, selected: false },
-        ],
-        votingDays: voteData.results,
-        person: voteData.person,
-        textFilter: '',
-        selectedOptions: [],
-      };
-    },
-    methods: {
-      toggleOption(optionId) {
-        const clickedOption = this.allOptions.filter(option => option.id === optionId)[0];
-        clickedOption.selected = !clickedOption.selected;
       },
     },
   });
